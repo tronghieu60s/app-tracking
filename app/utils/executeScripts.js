77d5e1402a88cdbb31aaeede3d31a4a1acbee6be
Trong Hieu <tronghieu60s@gmail.com>
2022-02-15 20:39:57 +0700

    update ghn

--- app/utils/executeScripts.js
+++ app/utils/executeScripts.js
@@ -1,10 +1,28 @@
+// Example: https://donhang.ghn.vn/?order_code=GANH64B4
+
 export const GHN_INJECTED_JAVASCRIPT = `(function() {
     setTimeout(async function() {
-      const packageCode = document.querySelector(".order-tracking-customer .header-order div:nth-child(1) .value > span").innerText;
-      const packageWeight = 0;
-      const packageLocation = 
+      const packageCode = document.querySelector('.order-tracking-customer .header-order div:nth-child(1) .value > span').innerText;
+      const packageLocation = document.querySelector('.order-tracking-customer .header-order div:nth-child(5) .value > div').innerText;
+
+      const timeline = document.querySelectorAll('.table-log-item');
+      const packageTimeline = [];
+      for (let i = timeline.length - 1; i >= 0; i -= 1) {
+        const getTime = timeline[i].parentElement
+          .previousElementSibling.querySelector(".table-col.block-center-between")
+          .innerText.split(",")?.[1].trim();
+        const labelTime = getTime + " " + timeline[i].querySelector('.table-row.block-align-top > div:nth-child(3)').innerText.trim();
+        const labelStatus = timeline[i].querySelector('.table-row.block-align-top > div:nth-child(2)').innerText.trim();
+        packageTimeline.push({labelTime, labelStatus});
+      }
+
+      const packageInfo = {packageCode, packageLocation, packageTimeline};
+      const packageInfoText = JSON.stringify(packageInfo);
+      window.ReactNativeWebView.postMessage(packageInfoText);
     }, 3000);
   })();`;
+
+// Example: http://www.vnpost.vn/vi-vn/dinh-vi/buu-pham?key=CE373808865VN
 
 export const VN_POST_INJECTED_JAVASCRIPT = `(function() {
     setTimeout(async function() {
@@ -17,7 +35,7 @@ export const VN_POST_INJECTED_JAVASCRIPT = `(function() {
       for (let i = timeline.length - 1; i >= 0; i -= 1) {
         const labelTime = timeline[i].querySelector('.label-time').innerText.trim();
         const labelLocation = timeline[i].querySelector('.block-span .label-location').innerText.trim();
-        const labelStatus = timeline[i].querySelector('.block-span').innerText.replace(labelLocation, "").trim();
+        const labelStatus = timeline[i].querySelector('.block-span').innerText.replace(labelLocation, '').trim();
         packageTimeline.push({labelTime, labelLocation, labelStatus});
       }
 
