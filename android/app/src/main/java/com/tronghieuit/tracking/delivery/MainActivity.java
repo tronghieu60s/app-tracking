343c05df5706f164165603cf2eca1c175bc92189
Trong Hieu <tronghieu60s@gmail.com>
2022-03-04 18:05:10 +0700

    config environtment

--- android/app/src/main/java/com/tronghieuit/tracking/delivery/MainActivity.java
+++ android/app/src/main/java/com/tronghieuit/tracking/delivery/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
       SplashScreen.show(this);
