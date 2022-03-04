cf2ae00e7abbdf0a6cbd3d08e0dbad082ab178e9
Trong Hieu <tronghieu60s@gmail.com>
2022-03-04 17:46:36 +0700

    update flash screen

--- android/app/src/main/java/com/tronghieuit/tracking/delivery/MainActivity.java
+++ android/app/src/main/java/com/tronghieuit/tracking/delivery/MainActivity.java
@@ -1,8 +1,8 @@
 package com.tronghieuit.tracking.delivery;
 
 import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -10,6 +10,12 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "PackageTracking";
